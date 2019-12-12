a84196cc99a87f15e6f06602630bf772323670e3
Ford K Yang <yk804031@126.com>
2019-12-12 23:49:48 +0800

    commit on master

---
+++ src/pages/demo/Life.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './../demo/life.less';
+class Life extends React.Component {
+    render() {
+        return (
+            <div>
+                <div className="content">
+                    this is life
+                </div>
+                <div className="content">
+                    this is two life
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Life;
